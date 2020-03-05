5a0181152a90883445f6f14abfef8722d2acde8f
mateusfg7 <mateusfelipefg77@gmail.com>
2020-03-05 16:36:57 -0300

    first code

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello Word!</Text>
+      <View style={styles.box} />
     </View>
   );
 }
@@ -16,4 +17,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  box: {
+    backgroundColor: '#000',
+    width: 60,
+    height: 60,
+  }
 });

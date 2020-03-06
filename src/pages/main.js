b6650f73607e78a3f9593c913aaa677cde9323e0
mateusfg7 <mateusfelipefg77@gmail.com>
2020-03-06 16:46:59 -0300

    import and use 'StyleSheet'

--- src/pages/main.js
+++ src/pages/main.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 import api from '../services/api'
 
-import { View, Text, FlatList, TouchableOpacity } from 'react-native'
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native'
 
 function Main() {
 
@@ -35,7 +35,7 @@ function Main() {
     return (
         <View style={styles.container}>
             <FlatList
-                contentContainerStyle={StyleSheet.list} 
+                contentContainerStyle={styles.list} 
                 data={documents.docs}
                 keyExtractor={item => item._id}
                 renderItem={item => renderItem(item)}
@@ -46,5 +46,55 @@ function Main() {
 Main.navigationOptions = {
     title: 'JSHunt',
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fafafa'
+    },
+    
+    list: {
+        padding: 20
+    },
+    
+    productContainer: {
+        backgroundColor: '#FFF',
+        borderWidth: 1,
+        borderColor: '#ddd',
+        borderRadius: 5,
+        padding: 20,
+        marginBottom: 20,
+    },
+    
+    productTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#333'
+    },
+
+    productDescription: {
+        fontSize: 16,
+        color: '#999',
+        marginTop: 5,
+        lineHeight: 24
+    },
+
+    productButton: {
+        height: 42,
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: '#DA552F',
+        backgroundColor: 'transparent',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10
+    },
+
+    productButtonText: {
+        fontSize: 16,
+        color: '#da552f',
+        fontWeight: 'bold'
+    }
+})
 
 export default Main
